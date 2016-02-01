dd14126031f117ca66ebc0065a6edbe894463c6f
arborist <mppmvk@web.de>
2016-02-01 19:34:03 +0100

    fix index

--- python/ant.py
+++ python/ant.py
@@ -1,6 +1,6 @@
 def execute(loc, target, path, ants):
     if len(path) > 0:
-        next_step = path[0]
+        next_step = path[1]
         walk_direction = ants.direction(loc, next_step)
         return walk_direction[0]
     else:
